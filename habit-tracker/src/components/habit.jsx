074f75da8950ad7b60715dd1cfb7f8069801f149
suu3 <ahob789@ewhain.net>
2022-03-04 09:02:20 +0900

    4.9 Event-1

--- habit-tracker/src/components/habit.jsx
+++ habit-tracker/src/components/habit.jsx
@@ -1,17 +1,14 @@
 import React, { Component } from 'react';
 
 class Habit extends Component {
-    state = {
-        count: 0,
-    };
-    handleIncrement = (event) => {
-        // state 오브젝트 안에 있는 count를 증가한 뒤 state를 업데이트 해야 함.
-        this.setState({count: this.state.count + 1});
+    handleIncrement = () => {
+        this.props.onIncrement(this.props.habbit);
     }
-    handleDecrement = (event) => {
-        // state 오브젝트 안에 있는 count를 증가한 뒤 state를 업데이트 해야 함.
-        const count = this.state.count - 1;
-        this.setState({count: count < 0 ? 0 : count});
+    handleDecrement = () => {
+        this.props.onDecrement(this.props.habbit);
+    }
+    handleDelete = () => {
+        this.props.onDelete(this.props.habbit);
     }
     render() {
         const { name, count } = this.props.habit;
@@ -25,7 +22,7 @@ class Habit extends Component {
                 <button className="habbit-button habit-decrease" onClick={this.handleDecrement}>
                     <i className="fas fa-minus-square"></i>
                 </button>
-                <button className="habbit-button habit-delete">
+                <button className="habbit-button habit-delete" onClick={this.handleDelete}>
                     <i className="fas fa-trash"></i>
                 </button>
             </li>
--- habit-tracker/src/components/habits.jsx
+++ habit-tracker/src/components/habits.jsx
@@ -9,11 +9,20 @@ class Habits extends Component {
             { id: 3, name: 'Coding', count: 0},
         ],
     }
+
+    handleIncrement = (habbit) => {
+    }
+    handleDecrement = (habbit) => {
+    }
+    handleDelete = (habbit) => {
+
+    }
+
     render() {
         return (
             <ul>
                 {this.state.habits.map(habit => (
-                    <Habit key={habit.id} habit={habit} />
+                    <Habit key={habit.id} habit={habit} onIncrement={this.handleIncrement} onDecrement={this.handleDecrement} onDelete={this.handleDelete} />
                 ))}                
             </ul>
         );
