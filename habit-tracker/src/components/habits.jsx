83b0760c61d0504d2d99b5617b21bd997488803e
suu3 <ahob789@ewhain.net>
2022-03-04 10:55:16 +0900

    4.11 Navbar

--- habit-tracker/src/components/habits.jsx
+++ habit-tracker/src/components/habits.jsx
@@ -1,42 +1,32 @@
-import React, { Component } from 'react';
-import Habit from './habit';
+import React, { Component } from "react";
+import Habit from "./habit";
 
 class Habits extends Component {
-    state = {
-        habits : [
-            { id: 1, name: 'Reading', count: 0},
-            { id: 2, name: 'Running', count: 0},
-            { id: 3, name: 'Coding', count: 0},
-        ],
-    }
+  handleIncrement = (habit) => {
+    this.props.onIncrement(habit);
+  };
+  handleDecrement = (habit) => {
+    this.props.onDecrement(habit);
+  };
+  handleDelete = (habit) => {
+    this.props.onDelete(habit);
+  };
 
-    handleIncrement = (habit) => {
-        const habits = [...this.state.habits];
-        const index = habits.indexOf(habit);
-        habits[index].count++;
-        this.setState({ habits });
-    }
-    handleDecrement = (habit) => {
-        const habits = [...this.state.habits];
-        const index = habits.indexOf(habit);
-        const count = habits[index].count - 1;
-        habits[index].count = count < 0 ? 0 : count;
-        this.setState({ habits });
-    }
-    handleDelete = (habit) => {
-        const habits = this.state.habits.filter(item => item.id !== habit.id);
-        this.setState({ habits });
-    }
-
-    render() {
-        return (
-            <ul>
-                {this.state.habits.map(habit => (
-                    <Habit key={habit.id} habit={habit} onIncrement={this.handleIncrement} onDecrement={this.handleDecrement} onDelete={this.handleDelete} />
-                ))}                
-            </ul>
-        );
-    }
+  render() {
+    return (
+      <ul>
+        {this.props.habits.map((habit) => (
+          <Habit
+            key={habit.id}
+            habit={habit}
+            onIncrement={this.handleIncrement}
+            onDecrement={this.handleDecrement}
+            onDelete={this.handleDelete}
+          />
+        ))}
+      </ul>
+    );
+  }
 }
 
 export default Habits;
