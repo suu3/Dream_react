3ad884d78e4e184706049e487d1f0091db0db6f9
suu3 <ahob789@ewhain.net>
2022-03-04 11:39:39 +0900

    4.13 Reset

--- habit-tracker/src/components/habits.jsx
+++ habit-tracker/src/components/habits.jsx
@@ -32,6 +32,9 @@ class Habits extends Component {
             />
           ))}
         </ul>
+        <button className="habits-reset" onClick={this.props.onReset}>
+          Reset All
+        </button>
       </>
     );
   }
