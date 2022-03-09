03fb77df6b1be49376ae1feb8fb1108b22eb6a13
suu3 <ahob789@ewhain.net>
2022-03-09 18:21:30 +0900

    [youtube] 8.7 search

--- youtube/src/app.jsx
+++ youtube/src/app.jsx
@@ -6,6 +6,23 @@ import VideoList from "./components/video_list/video_list";
 function App() {
   const [videos, setVideos] = useState([]);
   const API_KEY = process.env.REACT_APP_API_KEY;
+  const search = (query) => {
+    const requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+
+    fetch(
+      `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResult=25&q=${query}&type=video&key=${API_KEY}`,
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) =>
+        result.items.map((item) => ({ ...item, id: item.id.videoId }))
+      )
+      .then((items) => setVideos(items))
+      .catch((error) => console.log("error", error));
+  };
   useEffect(() => {
     const requestOptions = {
       method: "GET",
@@ -25,7 +42,7 @@ function App() {
 
   return (
     <div className={styles.app}>
-      <SearcHeader />
+      <SearcHeader onSearch={search} />
       <VideoList videos={videos} />
     </div>
   );
--- youtube/src/components/search_header/search_header.jsx
+++ youtube/src/components/search_header/search_header.jsx
@@ -1,17 +1,43 @@
-import React from "react";
+import React, { useRef } from "react";
 import styles from "./search_header.module.css";
 
-const SearcHeader = (props) => (
-  <header className={styles.header}>
-    <div className={styles.logo}>
-      <img className={styles.img} src="/images/logo.png" alt="logo" />
-      <h1 className={styles.title}>Youtube</h1>
-    </div>
-    <input className={styles.input} type="search" placeholder="search..." />
-    <button className={styles.button} type="submit">
-      <img className={styles.buttonImg} src="/images/search.png" alt="search" />
-    </button>
-  </header>
-);
+const SearcHeader = ({ onSearch }) => {
+  const inputRef = useRef();
+  const handleSearch = () => {
+    const value = inputRef.current.value;
+    onSearch(value);
+  };
+  const onClick = () => {
+    handleSearch();
+  };
+  const onKeyPress = (event) => {
+    if (event.key === "Enter") {
+      handleSearch();
+    }
+  };
+  return (
+    <header className={styles.header}>
+      <div className={styles.logo}>
+        <img className={styles.img} src="/images/logo.png" alt="logo" />
+        <h1 className={styles.title}>Youtube</h1>
+      </div>
+      <input
+        ref={inputRef}
+        className={styles.input}
+        type="search"
+        placeholder="search..."
+        onKeyPress={onKeyPress}
+      />
+      <button className={styles.button} type="submit">
+        <img
+          className={styles.buttonImg}
+          src="/images/search.png"
+          alt="search"
+          onClick={onClick}
+        />
+      </button>
+    </header>
+  );
+};
 
 export default SearcHeader;
