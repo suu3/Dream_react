a8f8a6613d09bbb32e1c540e9a0ec09c13ec1d6a
suu3 <ahob789@ewhain.net>
2022-03-10 11:20:55 +0900

    [youtube] 8.8 search - refactoring

--- youtube/src/app.jsx
+++ youtube/src/app.jsx
@@ -3,43 +3,19 @@ import styles from "./app.module.css";
 import SearcHeader from "./components/search_header/search_header";
 import VideoList from "./components/video_list/video_list";
 
-function App() {
+function App({ youtube }) {
   const [videos, setVideos] = useState([]);
-  const API_KEY = process.env.REACT_APP_API_KEY;
   const search = (query) => {
-    const requestOptions = {
-      method: "GET",
-      redirect: "follow",
-    };
-
-    fetch(
-      `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResult=25&q=${query}&type=video&key=${API_KEY}`,
-      requestOptions
-    )
-      .then((response) => response.json())
-      .then((result) =>
-        result.items.map((item) => ({ ...item, id: item.id.videoId }))
-      )
-      .then((items) => setVideos(items))
-      .catch((error) => console.log("error", error));
+    youtube
+      .search(query) // 주석 처리시 프리티어 자동 포맷 X
+      .then((videos) => setVideos(videos));
   };
+
   useEffect(() => {
-    const requestOptions = {
-      method: "GET",
-      redirect: "follow",
-    };
-
-    fetch(
-      `https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=${API_KEY}`,
-      requestOptions
-    )
-      .then((response) => response.json())
-      .then((result) => {
-        setVideos(result.items);
-      })
-      .catch((error) => console.log("error", error));
+    youtube
+      .mostPopular() //
+      .then((videos) => setVideos(videos));
   }, []);
-
   return (
     <div className={styles.app}>
       <SearcHeader onSearch={search} />
--- youtube/src/index.js
+++ youtube/src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './app';
+import Youtube from './service/youtube';
 
+const API_KEY = process.env.REACT_APP_API_KEY;
+const youtube = new Youtube(API_KEY);
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App youtube={youtube}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
