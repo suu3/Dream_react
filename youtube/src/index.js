dcbae6c731ee6adc7f7549e543e5eb86fd2e540c
suu3 <ahob789@ewhain.net>
2022-03-10 13:45:41 +0900

    [youtube] 8.14 fetch->axios

--- youtube/src/index.js
+++ youtube/src/index.js
@@ -3,9 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './app';
 import Youtube from './service/youtube';
+import axios from 'axios';
 
 const API_KEY = process.env.REACT_APP_API_KEY;
-const youtube = new Youtube(API_KEY);
+const httpClient = axios.create({
+  baseURL: 'https://www.googleapis.com/youtube/v3',
+  params: { key: API_KEY },
+});
+
+const youtube = new Youtube(httpClient);
+
 ReactDOM.render(
   <React.StrictMode>
     <App youtube={youtube}/>
--- youtube/src/service/youtube.js
+++ youtube/src/service/youtube.js
@@ -1,28 +1,28 @@
 class Youtube {
-    constructor(key) {
-        this.key = key;
-        this.getRequestOptions = {
-            method: "GET",
-            redirect: "follow",
-        };
+    constructor(httpClient) {
+        this.youtube = httpClient;
     }
 
     async mostPopular() {
-        const response = await fetch(
-            `https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=${this.key}`,
-            this.getRequestOptions
-        );
-        const result_1 = await response.json();
-        return result_1.items;
+        const response = await this.youtube.get('videos', {
+            params: {
+                part: 'snippet',
+                chart: 'mostPopular',
+                maxResults: 25,
+            }
+        });
+        return response.data.items; // 자동 json 변환
     }
 
     async search(query) {
-        const response = await fetch(
-            `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResult=25&q=${query}&type=video&key=${this.key}`,
-            this.getRequestOptions
-        );
-        const result_1 = await response.json();
-        return result_1.items.map((item) => ({ ...item, id: item.id.videoId }));
+        const response = await this.youtube.get('videos', {
+            params: {
+                part: 'snippet',
+                chart: 'mostPopular',
+                maxResults: 25,
+            }
+        });
+        return response.data.items.map((item) => ({ ...item, id: item.id.videoId }));
     }
 }
 
