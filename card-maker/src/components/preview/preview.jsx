bc8c42c116e364e11274eb965c15514f08562214
suu3 <ahob789@ewhain.net>
2022-03-11 16:18:18 +0900

    [card-maker] 10.10 maker card UI

--- card-maker/src/components/preview/preview.jsx
+++ card-maker/src/components/preview/preview.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import styles from "./preview.module.css";
 import Card from "../card/card";
+
 const Preview = ({ cards }) => {
   return (
     <section className={styles.preview}>
       <h1 className={styles.title}>Card Preview</h1>
-      {cards.map((card) => (
-        <Card card={card}></Card>
-      ))}
+      <ul className={styles.cards}>
+        {cards.map((card) => (
+          <Card card={card}></Card>
+        ))}
+      </ul>
     </section>
   );
 };
