65cb980db28500a5bb023252019545639f92b645
suu3 <ahob789@ewhain.net>
2022-03-11 12:12:21 +0900

    [card-maker] 10.3 login UI

--- card-maker/src/app.jsx
+++ card-maker/src/app.jsx
@@ -1,7 +1,11 @@
-import "./app.css";
+import styles from "./app.module.css";
 import Login from "./components/login/login";
 function App({ authService }) {
-  return <Login authService={authService} />;
+  return (
+    <div className={styles.app}>
+      <Login authService={authService} />
+    </div>
+  );
 }
 
 export default App;
