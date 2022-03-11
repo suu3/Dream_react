abe59816f203c2b6a057aefbcedc0da183f8dd8c
suu3 <ahob789@ewhain.net>
2022-03-11 15:03:29 +0900

    [card-maker] route & logout

---
+++ card-maker/src/components/maker/maker.jsx
@@ -0,0 +1,28 @@
+import React, { useEffect } from "react";
+import styles from "./maker.module.css";
+import Header from "../header/header";
+import Footer from "../footer/footer";
+import { useHistory } from "react-router-dom";
+
+const Maker = ({ authService }) => {
+  const history = useHistory();
+  const onLogout = () => {
+    authService.logout();
+  };
+
+  useEffect(() => {
+    authService.onAuthChange((user) => {
+      if (!user) {
+        history.push("/"); // 홈으로
+      }
+    });
+  });
+  return (
+    <section className={styles.maker}>
+      <Header onLogout={onLogout} />
+      <Footer />
+    </section>
+  );
+};
+
+export default Maker;
