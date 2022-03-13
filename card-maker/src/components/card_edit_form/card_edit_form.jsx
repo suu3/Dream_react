a5fd85ea2c07f229e44b7ed02fd4f40734564074
suu3 <ahob789@ewhain.net>
2022-03-13 13:25:40 +0900

    [card-maker] 10.15 state update

--- card-maker/src/components/card_edit_form/card_edit_form.jsx
+++ card-maker/src/components/card_edit_form/card_edit_form.jsx
@@ -3,27 +3,64 @@ import styles from "./card_edit_form.module.css";
 import Button from "../button/button";
 import ImageFileInput from "../image_file_input/image_file_input";
 
-const CardEditForm = ({ card }) => {
+const CardEditForm = ({ card, updateCard, deleteCard }) => {
   const { name, company, title, email, message, theme, fileName, fileURL } =
     card;
-  const onSubmit = () => {};
+  const onSubmit = () => {
+    deleteCard(card);
+  };
+  const onChange = (event) => {
+    if (event.currentTarget == null) {
+      return;
+    }
+    event.preventDefault();
+    updateCard({
+      ...card,
+      [event.currentTarget.name]: event.currentTarget.value,
+    });
+  };
+
   return (
     <form className={styles.form}>
-      <input className={styles.input} type="text" name="name" value={name} />
+      <input
+        onChange={onChange}
+        className={styles.input}
+        type="text"
+        name="name"
+        value={name}
+      />
       <input
         className={styles.input}
         type="text"
         name="company"
         value={company}
       />
-      <select className={styles.select} name="theme" value={theme}>
+      <select
+        onChange={onChange}
+        className={styles.select}
+        name="theme"
+        value={theme}
+      >
         <option value="light">light</option>
         <option value="dark">dark</option>
         <option value="colorful">colorful</option>
       </select>
-      <input className={styles.input} type="text" name="title" value={title} />
-      <input className={styles.input} type="text" name="email" value={email} />
+      <input
+        onChange={onChange}
+        className={styles.input}
+        type="text"
+        name="title"
+        value={title}
+      />
+      <input
+        onChange={onChange}
+        className={styles.input}
+        type="text"
+        name="email"
+        value={email}
+      />
       <textarea
+        onChange={onChange}
         className={styles.textarea}
         name="message"
         value={message}
