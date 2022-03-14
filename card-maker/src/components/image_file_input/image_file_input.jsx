2481e9197861e0fd145087ff64dcdcbca8f160fc
suu3 <ahob789@ewhain.net>
2022-03-14 11:38:02 +0900

    [card-maker] 10.19 image upload - loading spinner

--- card-maker/src/components/image_file_input/image_file_input.jsx
+++ card-maker/src/components/image_file_input/image_file_input.jsx
@@ -1,7 +1,8 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import styles from "./image_file_input.module.css";
 
 const Image_file_input = ({ imageUploader, name, onFileChange }) => {
+  const [loading, setLoading] = useState(false);
   const inputRef = useRef();
   const onButtonClick = (event) => {
     event.preventDefault();
@@ -9,8 +10,10 @@ const Image_file_input = ({ imageUploader, name, onFileChange }) => {
   };
 
   const onChange = async (event) => {
-    console.log(event.target.files[0]);
+    setLoading(true);
+    //    console.log(event.target.files[0]);
     const uploaded = await imageUploader.upload(event.target.files[0]);
+    setLoading(false);
     onFileChange({
       name: uploaded.original_filename,
       url: uploaded.url,
@@ -28,9 +31,15 @@ const Image_file_input = ({ imageUploader, name, onFileChange }) => {
         onChange={onChange}
         defaultValue=""
       />
-      <button className={styles.button} onClick={onButtonClick}>
-        {name || "No file"}
-      </button>
+      {!loading && (
+        <button
+          className={`${styles.button} ${name ? styles.pink : styles.grey}`}
+          onClick={onButtonClick}
+        >
+          {name || "No file"}
+        </button>
+      )}
+      {loading && <div className={styles.loading}></div>}
     </div>
   );
 };
